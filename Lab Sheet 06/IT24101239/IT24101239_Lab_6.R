c79a6a2b18bf4b7d5046cd913e3b5065538eefe0
IT24101239 <IT24101239@my.sliit.lk>
2025-09-25 18:20:04 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24101239/IT24101239_Lab_6.R
@@ -0,0 +1,18 @@
+setwd("C:\\Users\\USER\\Desktop\\IT24101239")
+
+#1
+#Binomial Distribution
+
+#2
+1-pbinom(47,50,0.85,lower.tail =FALSE)
+
+
+#1
+#number of customer calls per hour
+
+#2
+#poisson Distribution
+
+#3
+dpois(15,12)
+
