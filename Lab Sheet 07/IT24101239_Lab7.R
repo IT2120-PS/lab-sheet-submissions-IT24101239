3ebacb2ebfd8e9042149968b479d1f568d96b37b
IT24101239 <IT24101239@my.sliit.lk>
2025-09-26 12:51:50 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24101239_Lab7.R
@@ -0,0 +1,14 @@
+setwd("C:\\Users\\USER\\Desktop\\IT24101239")
+#1)
+punif(25,min=0,max=40,lower.tail = TRUE) - punif(10,min=0,max=40,lower.tail = TRUE)
+
+#2)
+pexp(2, rate = (1/3), lower.tail = TRUE)
+
+#3)
+#i)
+1 - pnorm(130, mean = 100, sd = 15, lower.tail = TRUE)
+
+#ii)
+qnorm(0.95, mean = 100, sd = 15, lower.tail = TRUE)
+
